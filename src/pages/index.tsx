1727966cbdaa347acae48949ef3643adf5d43718
smasiu <szymon.masko32@gmail.com>
2020-07-11 11:17:28 +0200

    social header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 import Layout from 'components/templates/layout/layout'
 import SEO from 'components/templates/seo/seo'
+import { Header } from 'components/header/header/header'
+import { headaerProps } from 'input/header.props'
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
+		<Header header={headaerProps} />
 	</Layout>
 )
 
