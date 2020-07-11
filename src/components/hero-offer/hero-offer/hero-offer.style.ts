e4b2f2161355a347deea6bf93d686425fdfd12dd
smasiu <szymon.masko32@gmail.com>
2020-07-11 21:31:10 +0200

    rwd hero offer

--- src/components/hero-offer/hero-offer/hero-offer.style.ts
+++ src/components/hero-offer/hero-offer/hero-offer.style.ts
@@ -11,8 +11,25 @@ export const HeroOfferContainer = styled.section`
 	position: relative;
 	top: -175px;
 	margin-bottom: -125px;
+	justify-items: center;
 	${({ theme }: ThemeProps) => css`
 		background-color: ${theme.colors.light100};
 		box-shadow: 0 -5px 10px ${theme.colors.shadow100};
 	`}
+
+	@media screen and (max-width: 1300px) {
+		width: 80%;
+		grid-template-columns: repeat(2, 1fr);
+	}
+
+	@media screen and (max-width: 800px) {
+		width: 90%;
+		grid-template-columns: 1fr;
+	}
+
+	@media screen and (max-width: 600px) {
+		top: 0;
+		margin-bottom: 50px;
+		margin-top: 50px;
+	}
 `
