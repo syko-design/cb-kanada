f64bf4681b4c9bf34965a0570246e6e3e8991364
smasiu <szymon.masko32@gmail.com>
2020-07-12 19:01:05 +0200

    dynamic-image

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
 import { FooterWrapper } from './footer.style'
-import { DynamicImage } from 'components/images/dynamic-image'
 
 export const Footer = () => <FooterWrapper>&copy; CB Canada 2020</FooterWrapper>
